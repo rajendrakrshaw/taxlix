8c6c0ce5183e209e9d23477e9848c243ffa47006
rajendrakrshaw <rajendra250601@gmail.com>
2022-06-24 19:09:09 +0530

    registration

--- verify_email.php
+++ verify_email.php
@@ -24,16 +24,18 @@ session_start();
   <?php 
     if($otp == $_GET['otp']){
       $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
+      $query = mysqli_query($conn, $q);
+      if($query){
       echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
      
       header("refresh:1;url=https://taxlix.rf.gd/login_form.php");
+      }
 
     }
     else{
       echo '<script> alert("Invalid OTP") </script>';
     }
 
-    if($_SESSION['logged'])
   ?>
 </body>
 </html>

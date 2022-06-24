f84c2ec4bd8ef96686421b6f57e51565de980b22
rajendrakrshaw <rajendra250601@gmail.com>
2022-06-24 23:49:46 +0530

    registration fix

--- verification.php
+++ verification.php
@@ -1,19 +1,19 @@
 <?php 
     session_start();
     $conn = mysqli_connect("sql207.epizy.com", "epiz_31860883", "AVfl0bjU2FPuOGv", "epiz_31860883_taxlix");
-    if($SESSION['otp'] == $_POST['otp']){
+    if(int($SESSION['otp']) == int($_POST['otp'])){
       $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
       $query = mysqli_query($conn, $q);
       
       echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
      
-      header("refresh:1;url=https://taxlix.rf.gd/login_form.php");
+      header("refresh:1;url=http://taxlix.rf.gd/login_form.php");
       
 
     }
     else{
-      echo '<script> alert("Invalid OTP") </script>';
-      header("refresh:1;url=https://taxlix.rf.gd/verify_email.php");
+      echo '<script> alert("Invalid OTP! OTP sent again.") </script>';
+      header("refresh:1;url=http://taxlix.rf.gd/verify_email.php");
     }
 
   ?>

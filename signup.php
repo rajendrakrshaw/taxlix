f987c0f4b0df78ebfdb3f1b5f4214929090f9d06
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-07 13:58:40 +0530

    db

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
     ob_start();
     session_start();
     // $conn = mysqli_connect("localhost", "root", "", "taxlix");
-    $conn = mysqli_connect("taxlix.com", "taxlixco_dev", "dev@taxlix", "taxlixco_taxlix");
+    $conn = mysqli_connect("localhost", "taxlixco_dev", "dev@taxlix", "taxlixco_taxlix");
 
     // $conn = mysqli_connect("sql207.epizy.com", "epiz_31860883", "AVfl0bjU2FPuOGv", "epiz_31860883_taxlix");
     if(isset($_GET['signup'])) {

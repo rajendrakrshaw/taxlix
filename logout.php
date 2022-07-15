21c3399e5223f2cdadf48ead70b4c080bdc7ae3d
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-16 00:09:14 +0530

    client dashboard

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ echo $_SESSION['user']['email'];
 
 $_SESSION['logged'] = false;
 echo $_SESSION['logged'];
-// session_abort();
+session_abort();
 // header("refresh:1;url=http://rajendrakrshaw.rf.gd/");
 
 

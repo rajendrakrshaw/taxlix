3a569372243cb6940a9f58b960cababbda4d2b9d
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-15 23:15:29 +0530

    client dashboard

--- login_form.php
+++ login_form.php
@@ -7,11 +7,11 @@
 <body>
 <div class="form-wrapper">
   
-  <form action="#" method="post">
+  <form action="login.php" method="post">
     <h3>Login here</h3>
 	
     <div class="form-item">
-		<input type="text" name="user" required="required" placeholder="Username" autofocus required></input>
+		<input type="text" name="email" required="required" placeholder="Email" autofocus required></input>
     </div>
     
     <div class="form-item">
@@ -22,28 +22,6 @@
 		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
     </div>
   </form>
-  <?php
-	if (isset($_POST['login']))
-		{
-			$username = mysqli_real_escape_string($con, $_POST['user']);
-			$password = mysqli_real_escape_string($con, $_POST['pass']);
-			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
-			$row		= mysqli_fetch_array($query);
-			$num_row 	= mysqli_num_rows($query);
-			
-			if ($num_row > 0) 
-				{			
-					$_SESSION['user_id']=$row['user_id'];
-					header('location:home.php');
-					
-				}
-			else
-				{
-					echo 'Invalid Username and Password Combination';
-				}
-		}
-  ?>
   <div class="reminder">
     <p>Not a member? <a href="signup_form.php">Sign up now</a></p>
     <p><a href="#">Forgot password?</a></p>

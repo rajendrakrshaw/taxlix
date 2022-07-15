0cb18b83fccfb1c4a6fff31f28eb3aacb62b29f0
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-15 22:51:33 +0530

    test

--- verification.php
+++ verification.php
@@ -1,20 +1,20 @@
 <?php 
     session_start();
     $conn = mysqli_connect("sql301.epizy.com", "epiz_32169318", "eyU7s1A71XX", "epiz_32169318_taxlix");
-    echo $_SESSION['otp'];
-    echo $_POST['otp'];
-    echo $_SESSION['email'];
-    echo $_SESSION['password'];
+    // echo $_SESSION['otp'];
+    $otp = $_POST['otp'];
+    $email =  $_SESSION['email'];
+    $password = $_SESSION['password'];
 
     if($_SESSION['otp'] == $_POST['otp']){
-      echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
-
-      // $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
-      // $query = mysqli_query($conn, $q);
-      
       // echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
+
+      $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
+      $query = mysqli_query($conn, $q);
+      // $q = "INSERT INTO `login`(`email`, `password`) VALUES ('$email','$password')";
+      echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
      
-      // header("refresh:1;url=http://taxlix.com/login_form.php");
+      header("refresh:1;url=http://taxlix.com/login_form.php");
       
 
     }

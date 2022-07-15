45477ab6cf7899eb013d31906755db3402e48886
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-15 15:33:36 +0530

    test

--- signup.php
+++ signup.php
@@ -10,7 +10,7 @@
         $q = "SELECT `email`, 'status' FROM `signup`";
         $emails = mysqli_query($conn, $q);
         $emails = $emails->fetch_array();
-        echo $emails;
+        print $emails;
         $_SESSION['notRegistered'] = true;
         foreach($emails as $mail){
             // echo "hhello";

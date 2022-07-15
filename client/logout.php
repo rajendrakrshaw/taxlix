253340a90e2585cb9922186b44295266335117b1
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-15 23:51:03 +0530

    client dashboard

--- client/logout.php
+++ client/logout.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $_SESSION['logged'] = false;
- 
+
 session_abort();
-header("refresh:1;url=https://taxlix.epizy.com/");
+header("refresh:1;url=https://rajenrakrshaw.rf.gd/");
 
 
 

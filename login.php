d5ad88ab4a61c09e44ecac3f84c20884de5041b7
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-15 23:21:29 +0530

    client dashboard

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php session_start();
 
 $_SESSION['logged'] = false;
- 
+$conn = mysqli_connect("sql301.epizy.com", "epiz_32169318", "eyU7s1A71XX", "epiz_32169318_taxlix");
+
 
 if (isset($_POST['login']))
 {
 	$username = mysqli_real_escape_string($con, $_POST['email']);
 	$password = mysqli_real_escape_string($con, $_POST['pass']);
 	
-	$query 		= mysqli_query($con, "SELECT * FROM signup WHERE  password='$password' and email='$username'");
+	$query 		= mysqli_query($con, "SELECT * FROM `signup` WHERE `password`='$password' and `email`='$username'");
 	$row		= mysqli_fetch_array($query);
 	$num_row 	= mysqli_num_rows($query);
 	

d0a854e544b24e3712ec4815a2176d9d056594c1
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-16 13:07:35 +0530

    reset pass

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-echo $_SESSION['logged'];
-echo $_SESSION['user']['email'];
+// echo $_SESSION['logged'];
+// echo $_SESSION['user']['email'];
 
 $_SESSION['logged'] = false;
-echo $_SESSION['logged'];
+// echo $_SESSION['logged'];
 session_destroy();
 header("refresh:1;url=http://rajendrakrshaw.rf.gd/");
 

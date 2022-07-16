83ce574c645842e0c688dbccd8b652accfbf3dab
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-16 11:58:12 +0530

    client dashboard

--- reset_password.php
+++ reset_password.php
@@ -8,24 +8,16 @@
 <div class="form-wrapper">
   
   <form action="login.php" method="post">
-    <h3>Login here</h3>
+    <h3>Enter Your Email</h3>
 	
     <div class="form-item">
 		<input type="text" name="email" required="required" placeholder="Email" autofocus required></input>
     </div>
-    
-    <div class="form-item">
-		<input type="password" name="pass" required="required" placeholder="Password" required></input>
-    </div>
-    
     <div class="button-panel">
 		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
     </div>
   </form>
-  <div class="reminder">
-    <p>Not a member? <a href="signup_form.php">Sign up now</a></p>
-    <p><a href="reset_password.php">Forgot password?</a></p>
-  </div>
+ 
   
 </div>
 

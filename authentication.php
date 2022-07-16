5d260ea3974dd10b063b3fa703ff55e17dc8f3e0
rajendrakrshaw <rajendra250601@gmail.com>
2022-07-16 12:35:55 +0530

    reset password

--- authentication.php
+++ authentication.php
@@ -9,12 +9,12 @@
     if($_SESSION['otp'] == $_POST['otp']){
       // echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
         include "new_password.php";
-      $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
-      $query = mysqli_query($conn, $q);
-      // $q = "INSERT INTO `login`(`email`, `password`) VALUES ('$email','$password')";
-      echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
-      session_abort();
-      header("refresh:1;url=http://taxlix.com/login_form.php");
+    //   $q = "INSERT INTO `signup`(`email`, `password`, `otp`, `status`) VALUES ('$email','$password','$otp','1')";
+    //   $query = mysqli_query($conn, $q);
+    //   // $q = "INSERT INTO `login`(`email`, `password`) VALUES ('$email','$password')";
+    //   echo '<script> alert("Registered Successfully! Please Go To Login Section.") </script>';
+    //   session_abort();
+    //   header("refresh:1;url=http://taxlix.com/login_form.php");
       
 
     }
